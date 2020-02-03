f98024f4ce5f6e53a94039e4e4b529414a181054
shdwdmn <shdwdmn@gmail.com>
2020-02-03 17:49:16 +0300

    refactor and comments

--- hello.py
+++ hello.py
@@ -1,27 +1,25 @@
 import pygame
 
-countdown = 50
+countdown = 50  # adjustable
 
-window_size = (500, 500)
-window_caption = "hello... pyg... ame... :=)"
-start_x = 250
-start_y = 400
-step = 150
 pig_color = (255, 80, 80)
 background_color = (50, 50, 50)
 black_color = (0, 0, 0)
 gold_color = (255, 215, 0)
+start_x = 250  # pig center position
+start_y = 400
+step = 150  # pig step
 pi = 3.141592653
 
 pygame.init()
-screen = pygame.display.set_mode(window_size)
-pygame.display.set_caption(window_caption)
+screen = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("hello... pyg... ame... :=)")
 
 font = pygame.font.Font('impact.ttf', 32)
 font_help = pygame.font.Font('impact.ttf', 18)
 text = font.render('Countdown: ', True, background_color, black_color)
 textRect = text.get_rect()
-textRect.center = (100, 50)
+textRect.center = (100, 50)  # text position (center)
 text_help = font_help.render('Play with arrows', True, black_color)
 helpRect = text_help.get_rect()
 helpRect.center = (70, 480)
@@ -51,7 +49,7 @@ y = start_y
 while not terminated and trigger < 2 and countdown > 0:
     pygame.time.delay(50)
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT:  # 'close' button event
             terminated = True
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
@@ -90,29 +88,31 @@ while not terminated and trigger < 2 and countdown > 0:
     text = font.render(f'Countdown: {countdown}', True, background_color, black_color)
     screen.fill(background_color)
     draw_pig(x, y)
-    pygame.draw.circle(screen, gold_color, (start_x, start_y - step*2), 48, 2)
-    screen.blit(text, textRect)
+    pygame.draw.circle(screen, gold_color, (start_x, start_y - step*2), 48, 2)  # gold circle
+    screen.blit(text, textRect)  # draw text and help text
     screen.blit(text_help, helpRect)
-    pygame.display.update()
+    pygame.display.update()  # redraw
 
-if terminated:
+if terminated:  # if window closed
     pygame.quit()
     quit()
 
-screen.fill(black_color)
+screen.fill(black_color)  # game over screen
 font = pygame.font.Font('impact.ttf', 64)
 textRect.center = (220, 250)
 if countdown > 0:
     text = font.render('You WIN!', True, pig_color, background_color)
     draw_pig(start_x, start_y - step*2)
     pygame.draw.circle(screen, gold_color, (start_x, start_y - step*2), 50, 4)
+    pygame.display.set_caption("Congratulations!")
 else:
     text = font.render('You LOSE!', True, background_color, black_color)
     draw_pig(start_x, start_y, reverse=True)
+    pygame.display.set_caption("Try again")
 screen.blit(text, textRect)
 pygame.display.update()
 
-while not terminated:
+while not terminated:  # wait for quit
     pygame.time.delay(200)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
