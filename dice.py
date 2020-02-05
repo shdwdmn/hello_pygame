48c639ae371b50c3f2010e778e3431a39441424d
shdwdmn <shdwdmn@gmail.com>
2020-02-05 10:22:59 +0300

    refactor

--- dice.py
+++ dice.py
@@ -1,3 +1,4 @@
+import os
 import pygame
 
 window_w = 800
@@ -7,42 +8,52 @@ score_color = (165, 165, 0)
 background_color = (200, 200, 255)
 font_path = 'fonts/Ritalin.ttf'
 
+os.environ['SDL_VIDEO_WINDOW_POS'] = "1120,180"
 pygame.init()
 screen = pygame.display.set_mode((window_w, window_h))
 pygame.display.set_caption("MAP")
 clock = pygame.time.Clock()
 
 
-class MainScreen:
-    def __init__(self):
-        pass
-
-
-class TextScreen:
-    def __init__(self):
-        pygame.draw.line(screen, black_color, (0, window_h/4*3), (window_w, window_h/4*3))
-
-
-def display_text(string):
-    pass
-
-
-class ScreenOneCharacter:
-    def __init__(self):
-        pass
-
-
-def draw_text(text, size, position=(150, 50), color=background_color, background=score_color):
+def draw_text(text, size, position, color, background):
     font = pygame.font.Font(font_path, size)
     text = font.render(text, True, color, background)
+    screen.blit(text, (position, (0, 0)))
     text_rect = text.get_rect()
-    text_rect.center = position
+    return text_rect
 
 
-def choose_your_character():
-    pass
+class Grid:
+    def __init__(self):
+        pass
 
-# def update_score(score):
+
+class MainScreen:
+    def __init__(self):
+        screen.fill(background_color)
+        pygame.draw.line(screen, black_color, (int(window_w/2), 0), (int(window_w/2), window_h))
+
+
+class Message(object):
+    def __init__(self):
+        self.position = (20, 500)
+
+    def display(self, string, size=32, color=black_color, background=None):
+        draw_text(string, size, self.position, color, background)
+
+
+class Score:
+    def __init__(self):
+        self.position = (30, 30)
+
+    def update(self, count):
+        draw_text(f'ОЧКОВ: {count}', 64, self.position, background_color, score_color)
+
+
+class Player:
+    def __init__(self):
+        player = pygame.image.load('images/chika.png')
+        screen.blit(player, (550, 500))
 
 
 terminated = False
@@ -57,23 +68,20 @@ while not terminated:  # main cycle
             terminated = True
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        x -= step
-    if keys[pygame.K_RIGHT]:
-        x += step
-    if keys[pygame.K_DOWN]:
-        y += step
-    if keys[pygame.K_UP]:
-        y -= step
+    # if keys[pygame.K_LEFT]:
+    #     x -= step
+    # if keys[pygame.K_RIGHT]:
+    #     x += step
+    # if keys[pygame.K_DOWN]:
+    #     y += step
+    # if keys[pygame.K_UP]:
+    #     y -= step
 
-    # text = font.render(f'ОЧКОВ: {score}', True, background_color, score_color)
-    screen.fill(background_color)
-    turtle = pygame.image.load('images/chika.png')
-    # pygame.draw.circle(screen, gold_color, (start_x, start_y - step*2), 48, 2)  # gold circle
-    screen.blit(turtle, (x, y))
-    # screen.blit(text, textRect)  # draw text and help text
-    # screen.blit(text_help, helpRect)
-    draw_text(f'ОЧКОВ: {score}', 64)
+    MainScreen.__init__(MainScreen())
+    Score.update(Score(), 0)
+    Player.__init__(Player())
+    Message.display(Message(), 'GAME OVER', 99, background=background_color)
+
     pygame.display.update()  # redraw
 
 # if terminated:  # if window closed
